a49519a98d4a60f86738933ea870c4a761d0810e
Parcon <jklcvb1@gmail.com>
2021-07-28 10:47:43 +0900

    falling

---
+++ Assets/GameScript/Falling_block.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Falling_block : MonoBehaviour
+{
+    Collider ObjectCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag.Equals("Ground"))
+        {
+            gameObject.GetComponent<Falling_block_Time>().enabled = true;
+        }
+    }
+}
---
+++ Assets/GameScript/Falling_block_Time.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Falling_block_Time : MonoBehaviour
+{
+    [SerializeField] float setTime = 3.0f;
+    public GameObject FallingBlock;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (setTime > 0)
+            setTime -= Time.deltaTime;
+        else if (setTime <=0 && setTime > -1)
+        {
+            Destroy(FallingBlock);
+        }
+    }
+
+}
