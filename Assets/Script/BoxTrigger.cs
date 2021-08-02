80e5d18e8b6e7c5aa475107f171b5ce8b3f13e36
urw7rs <urw.7rs@icloud.com>
2021-08-02 21:53:02 +0900

    add box that needs to be pushed by multiple objects

---
+++ Assets/Script/BoxTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxTrigger : MonoBehaviour
+{
+    public int count;
+    private bool move;
+
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.contactCount >= count)
+        {
+            rb.isKinematic = false;
+        }
+        else
+        {
+            rb.isKinematic = true;
+        }
+    }
+}
